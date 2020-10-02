e4e19eb53e19b77e93ad4d049292c4f77f46d40f
Travis Lee <travisslee223@gmail.com>
2020-10-01 21:08:46 -0400

    commit

--- lib/api.rb
+++ lib/api.rb
@@ -1,9 +1,10 @@
 class API
   def self.gets_pokemon(number)
-    url = "https://pokeapi.co/api/v2/pokemon/#{number}/"
+    url = "https://pokeapi.co/api/v2/pokemon-species//#{number}/"
     uri = URI(url)
     response = Net::HTTP.get(uri)
-    pokemon = JSON.parse(response) 
+    binding.pry
+    pokemon = JSON.parse(response)
   end
 
 end
--- lib/poke.rb
+++ lib/poke.rb
@@ -1,14 +1,21 @@
 class Pokemon
 
-  attr_accessor :name, :number
+  attr_accessor :name, :number, :description, :type1, :type2
 
   @@all = []
 
-  def initialize(name)
+  def initialize(name: nil, number: nil, description: nil, type1: nil, type2: nil)
+    @name = name
+    @number = number
+    @description = number
     @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def self.find_by_number(number)
+    self.all.select {|p| p.number == number}
   end
 end
