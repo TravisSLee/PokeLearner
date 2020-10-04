6eccb2b6060c5837bbe545023ccc50aee9ee3932
Travis Lee <travisslee223@gmail.com>
2020-10-03 21:00:47 -0400

    commit

--- lib/api.rb
+++ lib/api.rb
@@ -1,11 +1,8 @@
 class API
   def self.gets_pokemon
     url = "https://pokeapi.co/api/v2/pokemon-species"
-
     uri = URI(url)
-
     response = Net::HTTP.get(uri)
-
     results = JSON.parse(response)["results"]
     results.each do |c|
       Pokemon.new(name: c["name"], url: c["url"])
@@ -21,10 +18,12 @@ class API
 
   def self.get_desription(poke)
     #use poke.url to acess another page of the api and get info to add to ths poke object
-    url = "https://pokeapi.co/api/v2/pokemon-species/#{poke}/"
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
-    results = JSON.parse(response)
-    
+    Pokemon.all.map {|p| p.name == poke}
+      url = p.url
+      uri = URI(url)
+      response = Net::HTTP.get(uri)
+      results = JSON.parse(response)
+      
+
   end
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,26 +1,54 @@
 class CLI
   def menu
       API.gets_pokemon
-      input = ""
-      while input != "exit"
-        puts ""
-        puts "Welcome to PokeLearner"
-        puts "______________________"
-        puts ""
-        puts "Here is a list of the available pokemon. Pick one please"
-        API.number_list
-        puts "Now, enter a number from 1 - 20 to learn about a certain Pokemon, or hit 'exit' to exit the app"
-        input = gets.strip
-        if input.to_i > 0 && input.to_i <=20
-        #reach into poke.all and get the pokemon asked for and print it out
-          API.get_description(input)
-        elsif input.to_i > 20
-          puts "Sorry, that number is unavailable and that will be updated later on!!"
-        elsif input.to_i < 0
-          puts "Please, enter in a postive number."
+      puts ""
+      puts "Welcome to PokeLearner"
+      puts "______________________"
+      puts ""
+      puts "Here is a list of the available pokemon. Pick one please"
+      API.number_list
+      prompt
+      input = gets.strip.downcase
+      #reach into poke.all and get the pokemon asked for and print it out
+      while input != 'exit'
+        if input == "description"
+          please_pick
+          input = gets.strip.downcase
+          #checks if it is a pokemon then puts the pokemon and a description
+        elsif input == "capture rate"
+          please_pick
+          input = gets.strip.downcase
+          ##checks if it is a pokemon then puts the pokemon and a capture rate
+        elsif input == "genus"
+          please_pick
+          input = gets.strip.downcase
+          #checks stuff
+        elsif input == "habitat"
+          please_pick
+          input = gets.strip.downcase
+          #checks more
+        elsif input == "Dex Number"
+          please_pick
+          input = gets.strip.downcase
+          #checks
         else
-          puts "Sorry, please put a number!!"
+          puts "Sorry, that Pokemon is unavailable at the moment, further research is needed!!"
         end
       end
+      puts "Thank you for coming today."
+      puts "Bye Bye!!"
+    end
+
+    def prompt
+      puts ""
+      puts "Now, what do you what to learn about a Pokemon? Or, hit 'exit' to exit the app."
+      puts ""
+    end
+
+    def please_pick
+      puts ""
+      puts "Please pick a Pokemon."
+      puts ""
+    end
   end
 end
